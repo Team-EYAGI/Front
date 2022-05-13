eb418bb8607009fd4cb5021bac21dd72ef1e469c
hyopp <khb4711@gmail.com>
2022-05-14 01:05:12 +0900

    페이지 정리

--- src/components/FundingCard.js
+++ src/components/FundingCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
@@ -9,14 +9,13 @@ import { actionCreators as getActions } from "../redux/modules/fund";
 import { useParams } from "react-router-dom";
 import Like from "../components/Like";
 
+
+
 const FundingCard = (props) => {
-  // console.log(props.fundcard);
   const dispatch = useDispatch();
   const [fundHeartBool, setFundHeartBool] = useState(true)
 
   const addLike = () => {
-    //로그인후가능한거넣기
-
     if (fundHeartBool == false){
       setFundHeartBool(true)
       dispatch(getActions.addLikeDB(fundHeartBool, fundingcard.fundId)); 
@@ -28,13 +27,17 @@ const FundingCard = (props) => {
 
   const fundingcard = props.fundcard;
   const fundId = fundingcard.fundId;
-  console.log(fundId)
+  const player = useRef();
 
+  useEffect(() => {
+    player.current.audio.current.pause();
+  }, [fundingcard]);
 
   return (
     <React.Fragment>
       <Wrap>
         <Player>
+          {/* 책 정보 */}
           <h3>{fundingcard.bookTitle}</h3>
           <h4>{fundingcard.author}</h4>
           <PlayerImg>
@@ -43,43 +46,25 @@ const FundingCard = (props) => {
                 <div id="img">
                   <img src={fundingcard.bookImg} />
                 </div>
+                {/* 오디오 플레이어 */}
                 <AudioPlayer
               className="audio"
               autoPlay={false}
               src={fundingcard.fundFile}
               volume={1}
-              // progressUpdateInterval
-              // onListen={()=>{}}
-              // ListenInterval
+              showJumpControls={false}
+              ref={player}
               onPlay={(e) => console.log("onPlay")}
-              // other props here
             />
               </div>              
             </ImgSt>
             
-          </PlayerImg>         
+          </PlayerImg>   
+          {/* 펀딩정보 */}
           <Info>
             <div id="Sell">{fundingcard.sellerName}</div>
-            <div className="heart">목표</div>
+            <div className="heart">목표 <br /> {fundingcard.fundingGoals}개</div>
             <Like fundingcard={fundingcard}/>
-            {/* <div className="heart"
-            onClick={addLike}
-            >하트</div> */}
-            {/* 온클릭 이벤트 : 좋아요 누르면 채워진 하트, 좋아요 취소하면 비워진 하트 기능
-          {(like && (
-            <AiFillHeart
-              size="28"
-              style={{ margin: "8px" }}
-              onClick={delLike}
-              color="red"
-            />
-          )) || (
-              <AiOutlineHeart
-                size="28"
-                style={{ margin: "8px" }}
-                onClick={addLike}
-              />
-            )} */}
           </Info>
           <div id="creator">{fundingcard.content}</div>
         </Player>
@@ -89,48 +74,47 @@ const FundingCard = (props) => {
 };
 
 const Wrap = styled.div`
-  width: 390px;
+  width: 340px;
   height: 645px;
   position: relative;
-  /* margin: 0 auto; */
   display: flex;
   background: #FFFFFF;
   border: 1px solid #EAEAEA;  
   border-radius: 15px;
   margin-bottom: 41px;
-  font-family: 'Pretendard';
+  margin-right: 20px;
   font-style: normal;
   font-size: 18px;
 `;
 
 const Player = styled.div`
-  width: 342px;
-  /* background-color: red; */
-
+  width: 310px;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
   align-items: center;
-
   position: relative;
   padding-bottom: 30px; 
   font-weight: 500;
   font-style: normal;
-  font-family: 'Pretendard';
   
 
   h3 {
-    width: 200px;
+    width: 390px;
+    height: 50px;
     float: left;
     margin: 17px 0px 7px 0px;
     font-size: 16px;
     display: flex;
     flex-direction: column;
     align-items: center;
+    white-space: nowrap;
+    overflow:hidden;
+    text-overflow: ellipsis;
   }
 
   h4 {
-    width: 200px;
+    width: 190px;
     float: left;
     margin: 0px 0px 16px 0px;
     font-size: 16px;
@@ -142,7 +126,7 @@ const Player = styled.div`
   }
 
   h5 {
-    width: 200px;
+    width: 190px;
     float: left;
     margin: 0px 0px 16px 0px;
     font-size: 16px;
@@ -150,11 +134,12 @@ const Player = styled.div`
   }
 
   #creator {
-    width: 322px;
+    width: 290px;
     height: 141px;
     border-radius: 10px;
     border: 1px solid #C4C4C4;
     padding: 10px;
+    
   }
 
   /* 오디오 플레이어 커스텀 */
@@ -163,6 +148,7 @@ const Player = styled.div`
     height: 80px;
     border-radius: 20px;
     background: none;
+    box-shadow: none;
 
     div {
       background: "white";
@@ -182,22 +168,17 @@ const Player = styled.div`
 const Info = styled.div`
   display: flex;
   flex-direction: row;
-  /* align-items: stretch; */
-  /* background-color: purple; */
   margin: 16px 0px;
   width: 100%;
   display: flex;
   justify-content: space-between;
 
   #Sell {
-    /* background-color: yellow; */
     border: 1px solid gray;
     border-radius: 10px;
     width: 158px;
     height: 60px;
-    /* float: left; */
-    /* padding: 10px; */
-    /* margin: 17px 0px 16px 0px; */
+    margin-right: 5px;
     font-size: 15 px;
     text-align: center;
     vertical-align: middle;
@@ -207,13 +188,11 @@ const Info = styled.div`
   }
 
   .heart {
-    /* background-color: yellowgreen; */
     border: 1px solid gray;
     border-radius: 10px;
     width: 85px;
     height: 60;
-    /* float: left; */
-    /* margin: 17px 0px 16px 0px; */
+    margin-right: 5px;
     font-size: 15 px;
     text-align: center;
     vertical-align: middle;
@@ -225,53 +204,50 @@ const Info = styled.div`
 `;
 
 const PlayerImg = styled.div`
-  width: 342px;
+  width: 310px;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
   align-items: center;
   position: relative;
-  /* background-color: yellow; */
-  /* padding-bottom: 30px; */
   font-family: noto-sans-cjk-kr, sans-serif;
   font-weight: 400;
   font-style: normal;
 `;
 
 const ImgSt = styled.div`
-  width: 342px;
+  width: 310px;
   height: 342px;
-  /* background-color: gray; */
-  /* background-image: ; */
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   background-repeat: no-repeat;
   background-size: cover;
   border-radius: 30px;
 
   #img_wrap {
-    width: 342px;
+    width: 310px;
     height: 342px;
     flex-direction: column;
     background: rgba(0, 0, 0, 0.4);
     backdrop-filter: blur(26px);
     display: flex;
-    justify-content: center;
+    justify-content: space-evenly;
     align-items: center;
     border-radius: 30px;
   }
 
   #img {
-    width: 148px;
-    height: 224px;
-    background-color: gray;
-
+    width: 130px;
+    min-height: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
     img {
       width: 100%;
-      height: 100%;
-    }
+      border-radius: 2px 10px 10px 2px;
   }
 `;
 
--- src/components/Like.js
+++ src/components/Like.js
@@ -7,7 +7,6 @@ import { actionCreators as getActions } from "../redux/modules/fund";
 import { useParams } from "react-router-dom";
 
 const Like = (props) => {
-    // console.log(props)
     const likeCnt = props.fundingcard.likeCnt
     console.log(likeCnt)
     const fundId = props.fundingcard.fundId
@@ -18,8 +17,6 @@ const Like = (props) => {
 
 
   const addLike = () => {
-    //로그인후가능한거넣기
-
     if (fundHeartBool == false) {
       setFundHeartBool(true);
       dispatch(getActions.addLikeDB(fundHeartBool, fundId));
@@ -32,25 +29,31 @@ const Like = (props) => {
     <React.Fragment>
       <Wrap
       onClick={addLike}
-      ><AiOutlineHeart /> {likeCnt}개</Wrap>
+      ><AiOutlineHeart id="icon"  size="40px"/>  <h4>{likeCnt}개</h4></Wrap>
     </React.Fragment>
   );
 };
 
 const Wrap = styled.div`
-  /* background-color: yellowgreen; */
   border: 1px solid gray;
   border-radius: 10px;
   width: 85px;
-  height: 60;
-  /* float: left; */
-  /* margin: 17px 0px 16px 0px; */
+  height: 60px;
   font-size: 15 px;
   text-align: center;
   vertical-align: middle;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  h4 {
+    width: 100%;
+  }
+
+  #icon {
+    margin-top: 15px;   
+  }
 `;
 
 export default Like;
